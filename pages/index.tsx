a7ef1f551f38ba154888cf6aab491f257829ab27
Maxim Dymchenko <likewagon@outlook.com>
2021-12-21 19:32:21 -0800

    remove tailwindcss footprint

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,20 @@ import { Counter } from "src/components/Counter";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>My Starter</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
+    <div>
+      <main>
+        <h1>
           Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a href="https://nextjs.org">
             My Starter
           </a>
         </h1>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-
-        </div>
-
         <Counter />
-
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer style={{marginTop: 40}}>
         <a
-          className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
